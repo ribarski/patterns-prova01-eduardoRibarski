f73a8152565b9ba094456c2d8e3c1961ad0e7a2d
Eduardo Ribarski Scalco <eduardo.8723@alunosatc.edu.br>
2025-09-17 19:17:34 -0300

    padrão02 factory

--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,49 @@
-console.log("Hello, World! 002");
+class Bicicleta {
+    move() {
+      return "Pedalando a bicicleta no pátio da UNISATC";
+    }
+  }
+  
+  class Patinete {
+    move() {
+      return "Andando de patinete pelo estacionamento da UNISATC";
+    }
+  }
+  
+  class Onibus {
+    move() {
+      return "Pegando o ônibus para chegar na UNISATC";
+    }
+  }
+  
+  
+  class TransporteFactory {
+    static criarTransporte(tipo) {
+      switch (tipo) {
+        case "bicicleta":
+          return new Bicicleta();
+        case "patinete":
+          return new Patinete();
+        case "onibus":
+          return new Onibus();
+        default:
+          throw new Error(`O tipo de transporte '${tipo}' não é suportado.`);
+      }
+    }
+  }
+  
+  
+  function main() {
+    const tipos = ["bicicleta", "onibus", "patinete", "carro"];
+  
+    tipos.forEach(tipo => {
+      try {
+        const transporte = TransporteFactory.criarTransporte(tipo);
+        console.log(transporte.move());
+      } catch (error) {
+        console.error(error.message); 
+      }
+    });
+  }
+  
+  main();
